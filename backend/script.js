cba89eebe2831b656c8a0267790af4b9af67387d
Mikhail Sergeev <90124193+red88july@users.noreply.github.com>
2024-01-17 11:52:17 +0600

    Add files via upload

--- backend/script.js
+++ backend/script.js
@@ -1,27 +1,27 @@
 const express = require('express');
+const Vigenere = require('caesar-salad').Vigenere;
+const bodyParser= require('body-parser');
 const app = express();
 const port = 8000;
 
-const Vigenere = require('caesar-salad').Vigenere;
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('<h1>This is a default route </h1>' + `<h4>On this route can't find any content</h4>`);
+app.post('/encode', (req, res) => {
+
+    const password = req.body.password;
+    const message = req.body.message;
+
+    const encryptPass = Vigenere.Cipher(password).crypt(message);
+    res.json({encoded: encryptPass});
 });
 
-app.get('/Hello', (req, res) => {
-    res.send('<h1>Hello!</h1>');
-});
+app.post('/decode', (req, res) => {
 
-app.get('/encode/:encode', (req, res) => {
-    const encodingPassword = req.params.encode;
-    const encryptPass = Vigenere.Cipher('password').crypt(encodingPassword);
-    res.send(encryptPass);
-});
+    const password = req.body.password;
+    const message = req.body.message;
 
-app.get('/decode/:decode', (req, res) => {
-    const decodingPassword = req.params.decode;
-    const decryptionPassword = Vigenere.Decipher('password').crypt(decodingPassword);
-    res.send(decryptionPassword);
+    const decryptionPassword = Vigenere.Decipher(password).crypt(message);
+    res.json({encoded: decryptionPassword});
 });
 
 app.listen(port, () => {
